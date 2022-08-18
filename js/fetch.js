341a61c5650e3a03b8e0c7e6ad36d9145ce4a3b2
Suslenkov59 <107192083+Suslenkov59@users.noreply.github.com>
2022-08-18 23:50:32 +0500

    tikets

--- js/fetch.js
+++ js/fetch.js
@@ -1,30 +1,45 @@
-/*
-fetch("http://localhost:3001/tickets1") //убери 1 чтобы сервер корректно заработал
-    .then(res => {
-        if (res.ok) {
-            return res.json()
-        } else {
-            console.log("ERROR")
-            throw Error
-        }
-    })
-    .then(data =>
-        document.getElementById('Tickets').innerHTML =
-            JSON.stringify(data)
-    ).catch(error => {
-        console.log(error)
-})*/
-
 async function getResponse() {
     let response = await fetch('http://localhost:3001/tickets')
-    let content = await response.json()
-    content = content.splice(0, 5)
+    let content = await response.json() //тип файла
+    content = content.splice(0, 5) // колличество выводов из массива
 
     let list = document.querySelector("#Tickets")
 
     let key;
     for (key in content) {
-        list.innerHTML += '<div class="Ticket">${content[key].price}</div>' //надо настроить вывод именно цены
+
+        list.innerHTML += `
+    <div class="Ticket">
+        <div class="price">${content[key].price} ₽</div>
+        <div class="space"></div>
+        <div class="logo"><img class="companyLogo" src="http://pics.avs.io/99/36/${content[key].carrier}.png"></div>
+        <div class="routeAcheadCities">
+            <div class="cities">${content[key].segments[0].origin} - ${content[key].segments[0].destination}</div>
+            <div class="citiesTime"></div>
+        </div>
+        <div class="routeAcheadTime">
+         <div class="vPuti">В пути</div>
+            <div class="vPutiTimes"></div>
+        </div>
+        <div class="routeAcheadStops">
+            <div class="stops1">пересадки</div>
+            <div class="stops2"></div>
+        </div>
+          <div class="routeBackCities">
+            <div class="citiesBack">${content[key].segments[1].origin} - ${content[key].segments[1].destination}</div>
+            <div class="citiesTimeBack"></div>
+        </div>
+        <div class="routeBackTime">
+            <div class="vPutiBack"></div>
+            <div class="vPutiTimesBack"></div>
+        </div>
+        <div class="routeBackStops">
+            <div class="stopsBack1"> пересадки</div>
+            <div class="stopsBack2"></div>
+        </div>
+    </div>
+      `
+        console.log(content[key])
     }
 }
 
